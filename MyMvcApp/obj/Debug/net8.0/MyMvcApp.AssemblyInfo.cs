0b848d6f0f65c2d06f5342e829919685295dfdff
Anupam Khargharia <kharghariaanupam07@gmail.com>
2024-02-24 09:00:37 -0500

    Search with box

--- MyMvcApp/obj/Debug/net8.0/MyMvcApp.AssemblyInfo.cs
+++ MyMvcApp/obj/Debug/net8.0/MyMvcApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0145ea401b4a2b4826d3d487b6ccd96a478b93b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
